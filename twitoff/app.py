ecdceb4ea444b986ce5fc856410a5f41b4f41092
MarianVinas <marian-vinas@lambdastudents.com>
2020-09-30 18:12:20 -0700

    add a new route for Tweet

--- twitoff/app.py
+++ twitoff/app.py
@@ -5,7 +5,7 @@ from .db_model import DB, User
 def create_app():
     '''Create and configure an instance of our Flask application'''
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////Users/marianvinas/DSPT7-Twitoff/twitoff.sqlite"  # for absolute path
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////Users/marianvinas/DSPT7-Twitoff/twitoff.sqlite"  # for absolute path- mac
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     DB.init_app(app)  # Connect Flask app to SQLAlchemy DB
 
@@ -20,5 +20,11 @@ def create_app():
         DB.session.commit()
 
         return f'{username} has been added to the DB!'
+
+    
+    @app.route('/<tweet>/<user_id>')
+    def tweet(tweet, user_id):
+        tweet = Tweet(tweet=tweet, user_id=user_id)
+        return f'{username} post a tweet!'
 
     return app
