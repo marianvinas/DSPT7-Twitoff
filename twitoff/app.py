129d46533834b6ad9743047bae829eea8f903476
MarianVinas <marian-vinas@lambdastudents.com>
2020-10-07 21:03:15 -0700

    add return in reset route

--- twitoff/app.py
+++ twitoff/app.py
@@ -51,6 +51,7 @@ def create_app():
     def reset():
         DB.drop_all()
         DB.create_all()
+        return render_template('base.html', title='All Tweets Updated!', users=User.query.all())
 
     @app.route('/update', methods=['GET'])
     def update():
